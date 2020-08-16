3d6a4e2601de08b3c81a0507df742e18e4e5a6da
wirelyre <wirelyre@gmail.com>
2020-08-15 21:25:22 -0400

    [Python] Add argv

--- python/topple.py
+++ python/topple.py
@@ -2,11 +2,16 @@ import sys
 
 from parse import parse
 from primitives import primitives
-from runtime import ArithPrim, Primitive, Stack
+from runtime import ArithPrim, Cell, Get, Primitive, Stack
+
+
+argv = bytearray()
+for arg in sys.argv[1:]:
+    argv.extend(bytes(arg, "ascii") + b"\0")
 
 
 source = sys.stdin.read()
-defs = {}
+defs = {"argv": Get(None, Cell(argv))}
 
 ast = parse("(stdin)", source, primitives, defs)
 
